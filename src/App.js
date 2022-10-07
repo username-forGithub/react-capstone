82ad72070b89a4a1264d76137ff8a18673a04220
usernameBakhtiyor <harhil46@gmail.com>
2022-10-07 15:07:20 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Listing from './pages/Listing';
 import Single from './pages/Single';
 import './App.css';
